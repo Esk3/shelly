559f5b91434ce115883ec417bc188007209bf68a
eske <eske.brun3@gmail.com>
2024-05-30 09:07:48 +0200

    new lines

--- src/main.rs
+++ src/main.rs
@@ -10,5 +10,5 @@ fn main() {
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
 
-    print!("{}: command not found", input);
+    print!("{}: command not found\n", input.trim_end_matches("\r\n"));
 }
