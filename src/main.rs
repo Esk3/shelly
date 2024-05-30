f0cac77cd26cfa1212cbbb72d72e6f6217ca2c21
eske <eske.brun3@gmail.com>
2024-05-30 11:17:56 +0200

    echo

--- src/main.rs
+++ src/main.rs
@@ -30,18 +30,24 @@ struct CommandInput<'a>(&'a [&'a str]);
 enum Command {
     Exit(ExitCode),
     NotFound(String),
+    Echo(String),
 }
 
 impl Command {
     pub fn extract(input: CommandInput) -> Self {
         match input.0[0] {
             "exit" => Self::Exit(ExitCode::Ok),
+            "echo" => Self::Echo(input.0[1].to_string()),
             _ => Self::NotFound(input.0.join(" ")),
         }
     }
     pub fn run(self) -> bool {
         match self {
             Self::Exit(_) => true,
+            Self::Echo(echo) => {
+                println!("{echo}");
+                false
+            }
             Self::NotFound(cmd) => {
                 println!("{cmd}: command not found");
                 false
