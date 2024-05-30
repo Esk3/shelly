70cd4192c936e96b00bb97493d0242e83511463a
eske <eske.brun3@gmail.com>
2024-05-30 13:44:48 +0200

    changes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#![warn(clippy::pedantic)]
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
@@ -11,62 +12,135 @@ fn repl() {
     let stdin = io::stdin();
     let mut input = String::new();
 
+    let commands = Commands::builder()
+        .add_command(Echo::handler)
+        .add_command(Exit::handler)
+        .add_command(Type::handler)
+        .add_command(NotFound::handler);
+
     while run {
         print!("$ ");
         io::stdout().flush().unwrap();
         stdin.read_line(&mut input).unwrap();
 
-        let i = &input.split_whitespace().collect::<Vec<&str>>();
-        let command_input = CommandInput(i);
-        let command = Command::extract(command_input);
-        run = !command.run();
+        let request = Request::from(input.as_str());
+        let response = commands.extract_command(&request);
+        run = !response.0.run();
 
         input.clear();
     }
 }
 
-struct CommandInput<'a>(&'a [&'a str]);
-
-enum Command {
-    Exit(ExitCode),
-    NotFound(String),
-    Echo(String),
-    Type(String),
-}
-
-impl Command {
-    pub fn extract(input: CommandInput) -> Self {
-        match input.0[0] {
-            "exit" => Self::Exit(ExitCode::Ok),
-            "echo" => Self::Echo(input.0[1..].join(" ")),
-            "type" => Self::Type(input.0.get(1).unwrap_or(&"").to_string()),
-            _ => Self::NotFound(input.0.join(" ")),
-        }
-    }
-    pub fn run(self) -> bool {
-        match self {
-            Self::Exit(_) => true,
-            Self::Echo(echo) => {
-                println!("{echo}");
-                false
-            }
-            Self::Type(cmd) => {
-                let input = vec![cmd.as_str()];
-                if let Self::NotFound(_) = Self::extract(CommandInput(&input)) {
-                    println!("{cmd} not found");
-                } else {
-                    println!("{cmd} is a shell builtin");
-                }
-                false
-            }
-            Self::NotFound(cmd) => {
-                println!("{cmd}: command not found");
-                false
-            }
-        }
+struct Exit(i32);
+impl Command for Exit {
+    fn run(&self) -> bool {
+        true
     }
 }
 
-enum ExitCode {
-    Ok = 0,
+struct Type(String);
+impl Command for Type {
+    fn run(&self) -> bool {
+        todo!()
+    }
+}
+
+impl Type {
+    pub fn handler(request: &Request) -> Option<Response> {
+        let Some(first) = request.0.first() else {
+            return None;
+        };
+        if *first != "type" {
+            return None;
+        }
+        Some(Response(Box::new(Self((*request.0.get(1)?).to_string()))))
+    }
+}
+
+impl Exit {
+    pub fn handler(request: &Request) -> Option<Response> {
+        let Some(first) = request.0.first() else {
+            return None;
+        };
+        if *first != "exit" {
+            return None;
+        }
+        Some(Response(Box::new(Self(0))))
+    }
+}
+
+struct NotFound(String);
+impl Command for NotFound {
+    fn run(&self) -> bool {
+        println!("{}: command not found", self.0);
+        false
+    }
+}
+impl NotFound {
+    pub fn handler(request: &Request) -> Option<Response> {
+        Some(Response(Box::new(Self(
+            (*request.0.first().unwrap()).to_string(),
+        ))))
+    }
+}
+
+struct Echo(String);
+impl Command for Echo {
+    fn run(&self) -> bool {
+        println!("{}", self.0);
+        false
+    }
+}
+impl Echo {
+    pub fn handler(request: &Request) -> Option<Response> {
+        let Some(first) = request.0.first() else {
+            return None;
+        };
+        if *first != "echo" {
+            return None;
+        }
+        Some(Response(Box::new(Self(request.0[1..].join(" ")))))
+    }
+}
+
+struct Request<'a>(Vec<&'a str>);
+impl<'a> From<&'a str> for Request<'a> {
+    fn from(value: &'a str) -> Self {
+        Self(value.split_whitespace().collect::<Vec<&str>>())
+    }
+}
+struct Response(Box<dyn Command>);
+
+trait Command {
+    fn run(&self) -> bool;
+}
+
+type HandlerType = Box<dyn Fn(&Request) -> Option<Response>>;
+
+struct CommandHandler(HandlerType);
+trait IntoCommandHandler {
+    fn into(self) -> CommandHandler;
+}
+impl<T> IntoCommandHandler for T
+where
+    T: Fn(&Request) -> Option<Response> + 'static,
+{
+    fn into(self) -> CommandHandler {
+        CommandHandler(Box::new(self))
+    }
+}
+
+struct Commands(Vec<CommandHandler>);
+
+impl Commands {
+    pub fn builder() -> Self {
+        Self(Vec::new())
+    }
+    pub fn add_command(mut self, cmd: impl IntoCommandHandler) -> Self {
+        self.0.push(cmd.into());
+        self
+    }
+    pub fn extract_command(&self, request: &Request) -> Response {
+        self.0.iter().find_map(|h| h.0(request)).unwrap()
+    }
 }
