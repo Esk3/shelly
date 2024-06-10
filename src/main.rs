97ead437d58414eea7f256c22c2c3812b660d959
eske <eske.brun3@gmail.com>
2024-06-10 13:32:08 +0200

    exit

--- src/main.rs
+++ src/main.rs
@@ -72,7 +72,7 @@ impl Commands {
 impl Default for Commands {
     fn default() -> Self {
         Self {
-            commands: vec![Box::new(Echo), Box::new(Type)],
+            commands: vec![Box::new(Echo), Box::new(Exit), Box::new(Type)],
         }
     }
 }
