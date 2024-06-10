0ab5898e492d66c4681979f89d2e09fe8bfaa115
eske <eske.brun3@gmail.com>
2024-06-10 10:46:01 +0200

    this is kinda rly bad

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use std::env;
 #[allow(unused_imports)]
+#[warn(clippy::pedantic)]
 use std::io::{self, Write};
-//#![warn(clippy::pedantic)]
 
 fn main() {
     repl();
@@ -18,6 +19,9 @@ fn repl() {
         .add_command(Type::handler)
         .add_command(NotFound::handler);
 
+    let path_env = env::var("PATH").unwrap();
+    let options = Options::new(&path_env, &commands);
+
     while run {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -25,15 +29,29 @@ fn repl() {
 
         let request = Request::from(input.as_str());
         let response = commands.extract_command(&request);
-        run = !response.0.run();
+        run = !response.0.run(&options);
 
         input.clear();
     }
 }
 
+struct Options<'a> {
+    path: Vec<&'a str>,
+    commands: &'a Commands,
+}
+
+impl<'a> Options<'a> {
+    pub fn new(path: &'a str, commands: &'a Commands) -> Self {
+        Self {
+            path: path.split(':').collect(),
+            commands,
+        }
+    }
+}
+
 struct Exit(i32);
 impl Command for Exit {
-    fn run(&self) -> bool {
+    fn run(&self, _: &Options) -> bool {
         if self.0 > 0 {
             println!("{}", self.0);
         }
@@ -43,27 +61,43 @@ impl Command for Exit {
 
 struct Type(String);
 impl Command for Type {
-    fn run(&self) -> bool {
-        println!("looking for {}", self.0);
+    fn run(&self, options: &Options) -> bool {
+        let request = Request::from(self.0.as_str());
+        if let Some(_cmd) = options.commands.0.iter().find(|h| h.0(&request).is_some()) {
+            println!("{} is a shell builtin", self.0);
+            return false;
+        }
+        if let Some(path) = options
+            .path
+            .iter()
+            .map(|path| format!("{}/{}", path, self.0))
+            .find(|path| std::fs::metadata(path).is_ok())
+        {
+            println!("{} is {}", self.0, path);
+            return false;
+        }
+        println!("{} not found", self.0);
         false
     }
 }
 
 impl Type {
     pub fn handler(request: &Request) -> Option<Response> {
-        let Some(first) = request.0.first() else {
+        let Some(first) = request.args.first() else {
             return None;
         };
         if *first != "type" {
             return None;
         }
-        Some(Response(Box::new(Self((*request.0.get(1)?).to_string()))))
+        Some(Response(Box::new(Self(
+            (*request.args.get(1)?).to_string(),
+        ))))
     }
 }
 
 impl Exit {
     pub fn handler(request: &Request) -> Option<Response> {
-        let Some(first) = request.0.first() else {
+        let Some(first) = request.args.first() else {
             return None;
         };
         if *first != "exit" {
@@ -75,7 +109,7 @@ impl Exit {
 
 struct NotFound(String);
 impl Command for NotFound {
-    fn run(&self) -> bool {
+    fn run(&self, _: &Options) -> bool {
         println!("{}: command not found", self.0);
         false
     }
@@ -83,40 +117,44 @@ impl Command for NotFound {
 impl NotFound {
     pub fn handler(request: &Request) -> Option<Response> {
         Some(Response(Box::new(Self(
-            (*request.0.first().unwrap()).to_string(),
+            (*request.args.first().unwrap()).to_string(),
         ))))
     }
 }
 
 struct Echo(String);
 impl Command for Echo {
-    fn run(&self) -> bool {
+    fn run(&self, _: &Options) -> bool {
         println!("{}", self.0);
         false
     }
 }
 impl Echo {
     pub fn handler(request: &Request) -> Option<Response> {
-        let Some(first) = request.0.first() else {
+        let Some(first) = request.args.first() else {
             return None;
         };
         if *first != "echo" {
             return None;
         }
-        Some(Response(Box::new(Self(request.0[1..].join(" ")))))
+        Some(Response(Box::new(Self(request.args[1..].join(" ")))))
     }
 }
 
-struct Request<'a>(Vec<&'a str>);
+struct Request<'a> {
+    args: Vec<&'a str>,
+}
 impl<'a> From<&'a str> for Request<'a> {
     fn from(value: &'a str) -> Self {
-        Self(value.split_whitespace().collect::<Vec<&str>>())
+        Self {
+            args: value.split_whitespace().collect::<Vec<&str>>(),
+        }
     }
 }
 struct Response(Box<dyn Command>);
 
 trait Command {
-    fn run(&self) -> bool;
+    fn run(&self, options: &Options) -> bool;
 }
 
 type HandlerType = Box<dyn Fn(&Request) -> Option<Response>>;
