43501c22729edb11c72e41464ad32a0f1149c1c1
eske <eske.brun3@gmail.com>
2025-01-22 23:09:04 +0100

    new fs in main

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-#[allow(unused_imports)]
+//#[allow(unused_imports)]
 use shell_starter_rust::{
     commands::ShellCommands,
-    io::StdIoStream,
-    shell::{EnvData, Shell, State},
+    shell::{EnvData, State},
     shell_handler::Handler,
 };
-use shell_starter_rust::{io::Terminal, text::AutoComplete};
+use shell_starter_rust::{io::Terminal, shell::OsShell, text::AutoComplete};
 
 fn main() {
     let dict = vec!["echo", "type", "exit"];
     Handler::new(
-        Shell::new(State::new(EnvData::env()), ShellCommands::default()),
+        OsShell::new(
+            State::new(EnvData::env()),
+            ShellCommands::default(),
+            shell_starter_rust::fs::OsFileSystem,
+        ),
         Terminal::create(AutoComplete::new(dict)).unwrap(),
     )
     .run();
